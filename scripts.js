f5246ca82b116f8cd0b2fdb91e7e56465020d8d4
Ermek Barmashev <barmashev48@gmail.com>
2020-12-14 00:01:54 -0500

    firebase added :fire:

--- scripts.js
+++ scripts.js
@@ -2,6 +2,21 @@ const app = {};
 
 app.init = function () {
   console.log("ready");
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyA9mWNpNHwf0857gkyO-hHbcHzeCGHNWvk",
+    authDomain: "portfolio-4b928.firebaseapp.com",
+    projectId: "portfolio-4b928",
+    storageBucket: "portfolio-4b928.appspot.com",
+    messagingSenderId: "114829706415",
+    appId: "1:114829706415:web:94b0da68cae9174db2c05a",
+    measurementId: "G-NQVCMJWL7P",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 };
 
 app.eventListeners = function () {};
