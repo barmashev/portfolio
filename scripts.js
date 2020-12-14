d6911bd5a705390548738826162ec3bb95c31461
Ermek Barmashev <barmashev48@gmail.com>
2020-12-13 23:11:43 -0500

    test:fist:

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-console.log("work");
+const app = {};
+
+app.init = function () {
+  console.log("ready");
+};
+
+app.eventListeners = function () {};
+
+$(function () {
+  app.init();
+  app.eventListeners();
+});
