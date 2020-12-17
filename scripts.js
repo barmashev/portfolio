e12822e6262bb62db6127c7f9b6f33b2d3ca22e7
Ermek Barmashev <barmashev48@gmail.com>
2020-12-17 09:28:01 -0500

    finished smooth scroll :rocket:

--- scripts.js
+++ scripts.js
@@ -4,10 +4,67 @@ app.init = function () {
   console.log("ready");
 };
 
-app.eventListeners = function () {};
+// winScroll < about && $(navHome).addClass("navActive");
+
+app.scroll = function (destination) {
+  $("html, body").animate(
+    { scrollTop: $(`.${destination}`).offset().top },
+    500
+  );
+};
+
+app.eventListeners = function () {
+  $(".navLink").on("click", (e) => {
+    e.preventDefault();
+    console.log("clicked");
+    app.scroll(e.target.dataset.destination);
+  });
+};
 
 $(function () {
   AOS.init();
   app.init();
   app.eventListeners();
+
+  $(window).scroll(function () {
+    const winScroll = $(window).scrollTop() + 100;
+    const header = $(".header").offset().top;
+    const about = $(".about").offset().top;
+    const skills = $(".skills").offset().top;
+    const projects = $(".projects").offset().top;
+    const contact = $(".contact").offset().top;
+    console.log(winScroll);
+    console.log(about / 2);
+
+    if (winScroll > about / 2) {
+      $(".header__nav").addClass("navContainerActive");
+    } else {
+      $(".header__nav").removeClass("navContainerActive");
+    }
+
+    if (winScroll < about) {
+      $(".navActive").removeClass("navActive");
+      $(".navHome").addClass("navActive");
+    }
+
+    if (winScroll >= about) {
+      $(".navActive").removeClass("navActive");
+      $(".navAbout").addClass("navActive");
+    }
+
+    if (winScroll >= skills) {
+      $(".navActive").removeClass("navActive");
+      $(".navSkills").addClass("navActive");
+    }
+
+    if (winScroll >= projects) {
+      $(".navActive").removeClass("navActive");
+      $(".navProjects").addClass("navActive");
+    }
+
+    if (winScroll >= contact) {
+      $(".navActive").removeClass("navActive");
+      $(".navContact").addClass("navActive");
+    }
+  });
 });
